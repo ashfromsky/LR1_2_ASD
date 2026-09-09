51157266547fb0cb395d4f20795e006d376e39ff
ashfromsky <gamertimka9@gmail.com>
2026-09-09 12:13:57 +0300

    feat(main.go): small fixes in printf

--- main.go
+++ main.go
@@ -3,25 +3,25 @@ package main
 import "fmt"
 
 func runProgram1(testVars []int) {
-	fmt.Printf("=== Програма 1 (Одиничні порівняння) ===")
+	fmt.Printf("=== Програма 1 (Одиничні порівняння) ===\n")
 	for _, x := range testVars {
 		y, ok := calcSimpleComparisons(float64(x))
 		if ok {
 			fmt.Printf("x = %3d  -->  y = %8.3f\n", x, y)
 		} else {
-			fmt.Printf("x = %3d  -->  функція не існує\n", x, y)
+			fmt.Printf("x = %3d  -->  функція не існує\n", x)
 		}
 	}
 }
 
 func runProgram2(testVars []int) {
-	fmt.Printf("=== Програма 2 (Булеві операції) ===")
+	fmt.Printf("=== Програма 2 (Булеві операції) ===\n")
 	for _, x := range testVars {
 		y, ok := calcBoolLogic(float64(x))
 		if ok {
 			fmt.Printf("x = %3d  -->  y = %8.3f\n", x, y)
 		} else {
-			fmt.Printf("x = %3d  -->  функція не існує\n", x, y)
+			fmt.Printf("x = %3d  -->  функція не існує\n", x)
 		}
 	}
 }
